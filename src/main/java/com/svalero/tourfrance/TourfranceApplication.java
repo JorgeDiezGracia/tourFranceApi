2a0ae00240c35f4b5ad6108cb1057c3ab56500a2
JorgeDiezGracia <diezgraciajorge@gmail.com>
2025-07-04 13:19:20 +0200

    Creamos proyecto

---
+++ src/main/java/com/svalero/tourfrance/TourfranceApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.tourfrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourfranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourfranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/tourfrance/TourfranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.tourfrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourfranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
