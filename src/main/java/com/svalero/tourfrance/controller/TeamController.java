9dbc7bac9f6bd547621b0abaee721ff00a4e17e1
JorgeDiezGracia <diezgraciajorge@gmail.com>
2025-07-18 08:21:54 +0200

    Añadimos CRUD para Team

--- src/main/java/com/svalero/tourfrance/controller/TeamController.java
+++ src/main/java/com/svalero/tourfrance/controller/TeamController.java
@@ -1,47 +1,111 @@
 package com.svalero.tourfrance.controller;
 
 import com.svalero.tourfrance.domain.Team;
+import com.svalero.tourfrance.domain.dto.TeamInDto;
+import com.svalero.tourfrance.domain.dto.TeamOutDto;
+import com.svalero.tourfrance.domain.dto.TeamRegistrationDto;
+import com.svalero.tourfrance.exception.CyclistNotFoundException;
 import com.svalero.tourfrance.exception.TeamNotFoundException;
+import com.svalero.tourfrance.domain.dto.ErrorResponse;
 import com.svalero.tourfrance.service.TeamService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TeamController {
 
     @Autowired
     private TeamService teamService;
-
+    private final Logger logger = LoggerFactory.getLogger(TeamController.class);
 
     @GetMapping("/teams")
-    public ResponseEntity<List<Team>> getAll() {
-        return new ResponseEntity<>(teamService.getAll(), HttpStatus.OK);
+    public ResponseEntity<List<TeamOutDto>> getAll(@RequestParam(value = "name", defaultValue = "") String name,
+                                                   @RequestParam(value = "country", defaultValue = "") String country,
+                                                   @RequestParam(value = "email", defaultValue = "") String email) {
+
+        logger.info("BEGIN getAll");
+        List<TeamOutDto> teams = teamService.getAll(name, country, email);
+        logger.info("END getAll");
+        return new ResponseEntity<>(teams, HttpStatus.OK);
     }
+
 
     @GetMapping("/teams/:teamId")
     public ResponseEntity<Team> getTeam(long teamId) throws TeamNotFoundException {
+        logger.info("BEGIN getTeam");
         Team team = teamService.get(teamId);
+        logger.info("END getTeam");
         return new ResponseEntity<>(team, HttpStatus.OK);
     }
 
     @PostMapping("/teams")
-    public ResponseEntity<Team> addTeam(@RequestBody Team team) {
-        Team newTeam = teamService.add(team);
+    public ResponseEntity<Team> addTeam(@Valid @RequestBody TeamRegistrationDto team) {
+
+        logger.info("BEGIN addTeam");
+    TeamOutDto newTeam = teamService.add(team);
+        logger.info("END addTeam");
         return new ResponseEntity<>(newTeam, HttpStatus.CREATED);
-    }
+
+}
+
+    @PutMapping("/teams/:teamId")
+    public ResponseEntity<TeamOutDto> modifyTeam(long teamId, @Valid @RequestBody TeamInDto team) throws TeamNotFoundException{
+    logger.info("BEGIN modifyTeam");
+    TeamOutDto modifiedTeam = teamService.modify(teamId, team);
+    logger.info("END modifyTeam");
+    return new ResponseEntity<>(modifiedTeam, HttpStatus.OK);
+        }
 
     @DeleteMapping("/teams/:teamId")
     public ResponseEntity<Void> removeTeam(long teamId) throws TeamNotFoundException{
-        teamService.remove(teamId);
-        return ResponseEntity.noContent().build();
+    logger.info("BEGIN removeTeam");
+    teamService.remove(teamId);
+    logger.info("END removeTeam");
+    return ResponseEntity.noContent().build();
     }
 
-    @ExceptionHandler
-    public ResponseEntity<Void> handleTeamNotFoundException(TeamNotFoundException exception) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @ExceptionHandler(CyclistNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCyclistNotFoundException(CyclistNotFoundException exception) {
+        ErrorResponse error = ErrorResponse.generalError(404, exception.getMessage());
+        logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTeamNotFoundException(TeamNotFoundException exception) {
+        ErrorResponse error = ErrorResponse.generalError(404, exception.getMessage());
+        logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> MethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(ErrorResponse.validationError(errors), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse error = ErrorResponse.generalError(500, "Internal Server Error");
+        logger.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/svalero/tourfrance/service/TeamService.java
+++ src/main/java/com/svalero/tourfrance/service/TeamService.java
@@ -1,8 +1,11 @@
 package com.svalero.tourfrance.service;
 
 import com.svalero.tourfrance.domain.Team;
+import com.svalero.tourfrance.domain.dto.*;
 import com.svalero.tourfrance.exception.TeamNotFoundException;
 import com.svalero.tourfrance.repository.TeamRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,11 +18,33 @@ public class TeamService {
 
     @Autowired
     private TeamRepository teamRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
+    public List<TeamOutDto> getAll(String name, String country, String email) {
+        List<Team> teamList;
 
-    public List<Team> getAll() {
-        List<Team> allTeams = teamRepository.findAll();
-        return allTeams;
+        if (name.isEmpty() && country.isEmpty() && email.isEmpty()) {
+            teamList = teamRepository.findAll();
+        } else if (name.isEmpty() && !country.isEmpty() && email.isEmpty()) {
+            teamList = teamRepository.findByCountry(country);
+        } else if (!name.isEmpty() && country.isEmpty() && email.isEmpty()) {
+            teamList = teamRepository.findByName(name);
+        } else if (name.isEmpty() && country.isEmpty() && !email.isEmpty()) {
+            teamList = teamRepository.findByEmail(email);
+        } else if (!name.isEmpty() && !country.isEmpty() && email.isEmpty()) {
+            teamList = teamRepository.findByNameAndCountry(name, country);
+        } else if (!name.isEmpty() && country.isEmpty() && !email.isEmpty()) {
+            teamList = teamRepository.findByNameAndEmail(name, email);
+        } else if (name.isEmpty() && !country.isEmpty() && !email.isEmpty()) {
+            teamList = teamRepository.findByCountryAndEmail(country, email);
+        } else {
+            teamList = teamRepository.findByNameAndCountryAndEmail(name, country, email);
+        }
+
+        List<TeamOutDto> teamOutDtos = modelMapper.map(teamList, new TypeToken<List<TeamOutDto>>() {}.getType());
+        return teamOutDtos;
+
     }
 
     public Team get(long id) throws TeamNotFoundException {
@@ -27,9 +52,29 @@ public class TeamService {
                 .orElseThrow(TeamNotFoundException::new);
     }
 
-    public Team add(Team team) {
-         return teamRepository.save(team);
+    public TeamOutDto add(TeamRegistrationDto teamInDto) {
+        Team team = modelMapper.map(teamInDto, Team.class);
+        Team newTeam = teamRepository.save(team);
+         return modelMapper.map(newTeam, TeamOutDto.class);
     }
+
+    public TeamOutDto modify(long teamId, TeamInDto teamInDto) throws TeamNotFoundException {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(TeamNotFoundException::new);
+
+        //Team team = teamRepository.findById(cyclistInDto.getTeamId())
+                //.orElseThrow(TeamNotFoundException::new);
+
+        //cyclist.setTeam(team);
+
+        modelMapper.map(teamInDto, team);
+        modelMapper.typeMap(TeamInDto.class, Team.class)
+                .addMappings(mapper -> mapper.skip(Team::setId));
+        modelMapper.map(teamInDto, team);
+        teamRepository.save(team);
+        return modelMapper.map(team, TeamOutDto.class);
+    }
+
 
     public void remove(long id) throws TeamNotFoundException{
         teamRepository.findById(id)
