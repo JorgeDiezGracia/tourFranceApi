ff7538096f2838b29945f2d858454f60671bca0f
JorgeDiezGracia <diezgraciajorge@gmail.com>
2025-07-24 22:03:27 +0200

    Añado Sponsor y SponsorController

--- src/main/java/com/svalero/tourfrance/domain/Sponsor.java
+++ src/main/java/com/svalero/tourfrance/domain/Sponsor.java
@@ -1,4 +1,36 @@
 package com.svalero.tourfrance.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="sponsors")
 public class Sponsor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String country;
+    @Column
+    private String email;
+    @Column
+    private int employees;
+    @Column
+    private float funding;
+    @Column(name="end_contract")
+    private LocalDate endContract;
+
+    @OneToMany(mappedBy = "team")
+    @JsonBackReference(value = "teams_sponsors")
+    private List<Sponsor> sponsors;
 }
--- src/main/java/com/svalero/tourfrance/domain/Team.java
+++ src/main/java/com/svalero/tourfrance/domain/Team.java
@@ -13,7 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="teams")
-
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
